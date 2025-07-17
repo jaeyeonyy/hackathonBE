3de5ded4f6ed6547426b180aef76ebe3ffd86a0f
jaeyeon <kasto6329@gmail.com>
2025-07-17 14:33:24 +0900

    :gear: Setting

---
+++ src/test/java/ac/kr/sku/software/hackathonbe/HackathonBeApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.kr.sku.software.hackathonbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonBeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
